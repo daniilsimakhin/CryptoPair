39a17607616fbc3db26b6880dde4cf418651ff9f
daniilSimakhin <danil.110@yandex.ru>
2022-05-22 19:19:06 +0700

    modified label text

--- CryptoPair/Controller/ViewController.swift
+++ CryptoPair/Controller/ViewController.swift
@@ -16,8 +16,7 @@ class ViewController: UIViewController {
     
     private let label: UILabel = {
         let label = UILabel()
-        label.text = "Сryptocurrency exchange"
-
+        label.text = "CryptoPair"
         label.font = .italicSystemFont(ofSize: 45)
         label.adjustsFontSizeToFitWidth = true
         label.textAlignment = .center
